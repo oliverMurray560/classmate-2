57411227e6c52e34d376c9ec04ab2e98be7641be
Oliver Murray <creeperman69@icloud.com>
2023-10-20 09:17:30 -0500

    Initial Commit

---
+++ classmate 2UITests/classmate_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  classmate_2UITestsLaunchTests.swift
+//  classmate 2UITests
+//
+//  Created by OLIVER MURRAY on 10/20/23.
+//
+
+import XCTest
+
+final class classmate_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
